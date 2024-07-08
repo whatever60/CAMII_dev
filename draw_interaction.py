69109155a0fa6b4c04d17b1dc7ec99f5f18da746
whatever60 <quym@foxmail.com>
2024-07-08 14:09:02 -0400

    change python to python3 in head line

--- draw_interaction.py
+++ draw_interaction.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Plot interaction plot for CAMII isolates.
 """
 
--- select_colonies.py
+++ select_colonies.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """This step selects a given number of colonies detected by the previous step from each 
     plate group.
